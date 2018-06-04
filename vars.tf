228fb1cdd35b5d2c12af4bdea8967d8ee279bb97
Ido Gan <idogann@gmail.com>
2018-06-04 21:18:59 +0300

    Add description

--- vars.tf
+++ vars.tf
@@ -6,4 +6,6 @@ variable "subnet_ext_id" {}
 variable "subnet_int_id" {}
 variable "ssh_key_name" {}
 variable "kibana_url" {}
-variable "elk_sg" {}
+variable "elk_sg" {
+  description = "existing ELK security group id"
+}
